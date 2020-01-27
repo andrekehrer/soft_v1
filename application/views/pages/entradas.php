011d8d72f40dd0b2d715801f5c0d18f5fed8bd74
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2020-01-27 23:23:17 +0000

    menu externo

--- application/views/pages/entradas.php
+++ application/views/pages/entradas.php
@@ -41,64 +41,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <div id="wrapper">
 
     <!-- Sidebar -->
-    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-      <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
-        <div class="sidebar-brand-icon rotate-n-15">
-          <i class="fas fa-laugh-wink"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">Meu Dinheiro</div>
-      </a>
-
-
-      <hr class="sidebar-divider my-0">
-
-       <li class="nav-item active">
-        <a class="nav-link" href="/entradas">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Entradas Fixas</span></a>
-        </li>
-      <!-- Nav Item - Dashboard -->
-      <li class="nav-item active">
-        <a class="nav-link" href="/categorias">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Categorias</span></a>
-        </li>
-        <!-- Divider -->
-        <hr class="sidebar-divider my-0">
-
-        <!-- Nav Item - Dashboard -->
-        <li class="nav-item active">
-          <a class="nav-link" href="/">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-          </li>
-
-          <!-- Divider -->
-          <hr class="sidebar-divider">
-          <li class="nav-item">
-            <a class="nav-link" href="charts.html">
-              <i class="fas fa-fw fa-chart-area"></i>
-              <span>Perfil</span></a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="charts.html">
-                <i class="fas fa-fw fa-chart-area"></i>
-                <span>Change password</span></a>
-              </li>
-
-
-              <!-- Divider -->
-              <hr class="sidebar-divider d-none d-md-block">
-
-              <!-- Sidebar Toggler (Sidebar) -->
-              <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-              </div>
-
-            </ul>
+    <?php require_once('menu.php') ?>
             <!-- End of Sidebar -->
 
             <!-- Content Wrapper -->
