8b7bb35ba0375214b2d4988f1c316269d24dfb07
Andre Kehrer <drekehrer@gmail.com>
2020-01-27 09:40:16 +0000

    Update login_model.php

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -1,11 +1,15 @@
-<?php 
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
  * 
  */
 class Login_model extends CI_Model
 {
-	
+	public function __construct()
+    {
+        parent::__construct();
+    }
+    
 	public function index(){
 		return;
 	}
