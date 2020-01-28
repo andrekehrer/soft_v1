0adc8f605fd306d4941f5293f577d1e059d8ec61
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2020-01-28 15:13:42 +0000

    Update menu.php

--- application/views/pages/menu.php
+++ application/views/pages/menu.php
@@ -8,52 +8,54 @@
         <div class="sidebar-brand-text mx-3">Meu Dinheiro</div>
       </a>
 
-
       <hr class="sidebar-divider my-0">
-      
+    
       <li class="nav-item active">
         <a class="nav-link" href="<?php echo base_url(); ?>/entradas">
           <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Entradas</span></a>
-        </li>
+          <span>Entradas Fixas</span></a>
+      </li>
+
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <a class="nav-link" href="<?php echo base_url(); ?>/categorias">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Categorias</span></a>
+      </li>
+
         <hr class="sidebar-divider my-0">
+
         <!-- Nav Item - Dashboard -->
         <li class="nav-item active">
-          <a class="nav-link" href="<?php echo base_url(); ?>/categorias">
+          <a class="nav-link" href="<?php echo base_url(); ?>">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Categorias</span></a>
-          </li>
-          <!-- Divider -->
-          <hr class="sidebar-divider my-0">
+            <span>Dashboard</span></a>
+        </li>
 
-          <!-- Nav Item - Dashboard -->
-          <li class="nav-item active">
-            <a class="nav-link" href="<?php echo base_url(); ?>">
-              <i class="fas fa-fw fa-tachometer-alt"></i>
-              <span>Dashboard</span></a>
-            </li>
+        <!-- Divider -->
+        <hr class="sidebar-divider">
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-            <li class="nav-item">
-              <a class="nav-link" href="<?php echo base_url(); ?>">
-                <i class="fas fa-fw fa-chart-area"></i>
-                <span>Perfil</span></a>
-              </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo base_url(); ?>">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Perfil</span></a>
+        </li>
 
-              <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>">
-                  <i class="fas fa-fw fa-chart-area"></i>
-                  <span>Change password</span></a>
-                </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo base_url(); ?>">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Change password</span></a>
+        </li>
 
 
-                <!-- Divider -->
-                <hr class="sidebar-divider d-none d-md-block">
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
 
-                <!-- Sidebar Toggler (Sidebar) -->
-                <div class="text-center d-none d-md-inline">
-                  <button class="rounded-circle border-0" id="sidebarToggle"></button>
-                </div>
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+          <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
 
-              </ul>
+      </ul>
