b802d6167f06c4e13019671846df5b49b49d13ea
Andre Kehrer <drekehrer@gmail.com>
2020-02-04 11:13:02 +0000

    Update saidas.php

--- application/views/pages/saidas.php
+++ application/views/pages/saidas.php
@@ -317,7 +317,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             // echo "<pre>";print_r($value['categoria'][0]->cor);echo "</pre>";
                             // echo "<tr style='background-color:".$value['categoria'][0]->cor."'>";
                             if($today_date == $value['data']){
-                              echo "TODAY D: ".$today_date;
+                              echo "TODAY D: ".$today_date + 1;
                               echo "BANCO D: ".$value['data'];
                               echo "<tr style='border: 2px red solid'>";
                             }else{
