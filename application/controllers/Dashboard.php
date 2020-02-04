b6bfcabaae865774783c53b106e4242631026e8d
Andre Kehrer <drekehrer@gmail.com>
2020-02-04 09:50:03 +0000

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -12,10 +12,30 @@ class Dashboard extends CI_Controller {
 
 		
 		$data['total_bill'] = $this->saidas_model->get_all_saidas_apagar();
-		$data['saidas_diarias'] = $this->saidas_model_v->get_all_saidas();
 		$data['categorias'] = $this->categorias_model->get_all_cats();
 		$data['total_des_fixa'] = $this->saidas_model->get_all_saidas_pagas();
-		$data['total_mes'] = $this->entradas_model->get_all_entradas();
+		
+
+
+
+
+
+		//$saidas_diarias = $this->saidas_model_v->get_all_saidas();
+		$total_mes = $this->entradas_model->get_all_entradas();
+
+		$total_mes_total = 0;
+        foreach ($total_mes as $key => $value) {
+          $total_mes_total = $total_mes_total + $value->valor;
+        }
+        $total_mes = '£'.number_format($total_mes_total, 2, ',', '.');
+
+
+
+
+
+
+        $data['total_mes'] = $total_mes;                          
+		$data['saidas_diarias'] = $this->saidas_model_v->get_all_saidas();
 		$data['title'] = "Dashboard - Meu Dinheiro";
 		$this->load->view('pages/dashboard', $data);
 	}
