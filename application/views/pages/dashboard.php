90a91e8914dc6141870d1ebf4327deffe12cc97b
Andre Kehrer <drekehrer@gmail.com>
2020-02-04 10:24:56 +0000

    Update dashboard.php

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -269,10 +269,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                       
 
                       <!-- Earnings (Monthly) Card Example -->
-                      <a href="<?php echo base_url()?>/saidas_v">
                         <div class="col-xl-3 col-md-6 mb-4">
                           <div class="card border-left-primary shadow h-100 py-2" style="border-left: .25rem solid #fb7777!important;">
                             <div class="card-body">
+                              <a href="<?php echo base_url()?>/saidas_v">
                               <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                   <div class="text-xs font-weight-bold text-primary text-uppercase mb-1" style="color:#fb7777!important;">Saidas diarias</div>
@@ -285,14 +285,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 </div>
                               </div>
                             </div>
+                            </a>
                           </div>
                         </div>
-                     </a>
+                     
                       
-                    <a href="<?php echo base_url()?>/saidas">
                       <div class="col-xl-3 col-md-6 mb-4">
                         <div class="card border-left-primary shadow h-100 py-2" style="border-left: .25rem solid #a72845!important;">
                           <div class="card-body">
+                            <a href="<?php echo base_url()?>/saidas">
                             <div class="row no-gutters align-items-center">
                               <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1" style="color: #a72845!important;">Despesas fixas</div>
@@ -305,10 +306,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <i class="fas fa-calendar fa-2x text-gray-300"></i>
                               </div>
                             </div>
+                             </a> 
                           </div>
                         </div>
                       </div>
-                    </a> 
                       <!-- Earnings (Monthly) Card Example -->
                       <div class="col-xl-3 col-md-6 mb-4">
                         <div class="card border-left-success shadow h-100 py-2">
