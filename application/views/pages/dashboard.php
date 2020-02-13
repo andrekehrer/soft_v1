c49ea623b350429e6d55143135b6932b2595aaee
Andre Kehrer <drekehrer@gmail.com>
2020-02-13 11:30:46 +0000

    Update dashboard.php

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -296,17 +296,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
             <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Entradas anuais</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">
-                        <?php echo $total_anual; ?>
+                  <a href="<?php echo base_url() ?>/saidas">
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Entradas anuais</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">
+                          <?php echo $total_anual; ?>
+                        </div>
+                      </div>
+                      <div class="col-auto">
+                        <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
                       </div>
                     </div>
-                    <div class="col-auto">
-                      <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
+                  </a>
                 </div>
               </div>
             </div>
