17a4ab0e082c2c81d2be363a373d5958c3dc82a8
Andre Kehrer <drekehrer@gmail.com>
2020-02-14 17:36:37 +0000

    Update Entradas_model.php

--- application/models/Entradas_model.php
+++ application/models/Entradas_model.php
@@ -14,6 +14,8 @@ class Entradas_model extends CI_Model
 
 	public function get_all_entradas()
 	{
+		$mes_corrente = date('m');
+		$this->db->where('MONTH(data)', $mes_corrente);
 		return $this->db->get("entradas")->result();
 	}
 }
