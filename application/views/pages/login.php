19e475078dc66a32fbef82f6740455a199839ede
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2020-02-29 16:19:51 +0000

    icon

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -14,7 +14,7 @@
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
+  <link rel="icon" type="image/png" href="https://www.meudinheiroweb.com.br/assets/img/favicon/32x32.png" sizes="32x32">
   <!-- Custom styles for this template-->
   <link href="<?php echo base_url(); ?>assets/css/sb-admin-2.min.css" rel="stylesheet">
 
